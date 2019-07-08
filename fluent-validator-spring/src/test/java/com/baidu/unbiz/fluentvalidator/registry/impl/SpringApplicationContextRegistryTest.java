8fd859089d77b065da725f71ddba0ad997424449
petershowx <xiaoyulong1988@gmail.com>
2019-07-08 17:30:53 +0800

    fluent-validator-spring\pom.xml fluent-validator-spring\src\test\java\com\baidu\unbiz\fluentvalidator\registry\impl\SpringApplicationContextRegistryTest.java pom.xml

--- fluent-validator-spring/src/test/java/com/baidu/unbiz/fluentvalidator/registry/impl/SpringApplicationContextRegistryTest.java
+++ fluent-validator-spring/src/test/java/com/baidu/unbiz/fluentvalidator/registry/impl/SpringApplicationContextRegistryTest.java
@@ -8,8 +8,9 @@ import javax.annotation.Resource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.baidu.unbiz.fluentvalidator.FluentValidator;
 import com.baidu.unbiz.fluentvalidator.Result;
@@ -20,8 +21,7 @@ import com.baidu.unbiz.fluentvalidator.registry.Registry;
 /**
  * @author zhangxu
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@RunWith(SpringRunner.class)
 public class SpringApplicationContextRegistryTest {
 
     @Resource
